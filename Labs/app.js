c7524f03462a710cb0cd3a05e66edf0526c36a5f
akilajones <122486894+akilajones@users.noreply.github.com>
2023-01-17 19:43:59 -0500

    Add files via upload

---
+++ Labs/app.js
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+  <title>Practice</title>
+
+  <style>
+    .nike-shoes{
+      font: bolder;
+      font-size: 30px;
+    }
+   
+  </style>
+  
+</head>
+  
+<body>
+  <a href="https://www.amazon.com/" target="_blank">Back to Amazon</a>
+  <p class="nike-shoes"><b>
+    Nike Black Running Shoes</b>
+  </p>
+  <p class="stock">
+    $39 - in stock.
+  </p>
+  <p class="delivery">
+    Free delivery tomorrow.
+  </p>
+  <button>Add to cart</button>
+  <button>Buy now</button>
+  
+  
+  
+</body>
+</html>
